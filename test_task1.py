10b3cc83ae8bce745e624229592667a1b18c9724
Arina Safiullina <31733496+arinasaf11@users.noreply.github.com>
2018-12-28 16:09:23 +0300

    Update test_task1.py

--- test_task1.py
+++ test_task1.py
@@ -4,6 +4,7 @@ import task1
 class test_task():
     def test_func(self):
       self.assertEqual(task1.func(2,'hex'), '0x2')
+    def test_func1(self):
       self.assertEqual(task1.func(2), 'два')
 
 if __name__ == '__main__':
